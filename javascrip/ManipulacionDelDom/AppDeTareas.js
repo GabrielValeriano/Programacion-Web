3aea1751775c2ae835453dfea24fb5cd67ef05c6
NahiaraCuella <nahiaracuella16@gmail.com>
2025-05-26 16:58:10 -0300

    Completamos la actividad 1 de la manipulacion del dom y avance con html

--- javascrip/ManipulacionDelDom/AppDeTareas.js
+++ javascrip/ManipulacionDelDom/AppDeTareas.js
@@ -15,6 +15,12 @@ function addtask() {
   //crear un elemento html
 
   const li = document.createElement("li");
-  li.textContent = obj.task;
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  li.textContent = obj.task; //asigna a text content del valor que captura de la task
+  li.append(checkbox);
+
   list.append(li);
 }
