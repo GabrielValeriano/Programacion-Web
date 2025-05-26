aa1f045ffb2d36c679544720b57d5d348be6c44d
GabrielValeriano <valerianog1205@gmail.com>
2025-05-26 12:02:03 -0300

    x

--- javascrip/metodos-arrays/ActividadMetodosArray.js
+++ javascrip/metodos-arrays/ActividadMetodosArray.js
@@ -3,10 +3,10 @@
 
 const inventario = [];
 
-inventario.push({nombre:"Espada",oro: 150,categoria :"Epica"});
-inventario.push({nombre: "Escudo",oro: 50,categoria: "Normal"});
-inventario.push({nombre: "Casco",oro: 200,categoria: "Especial"});
-inventario.push({nombre: "Lanza",oro: 250,categoria: "Legendario"});
+inventario.push({ nombre: "Espada", oro: 150, categoria: "Epica" });
+inventario.push({ nombre: "Escudo", oro: 50, categoria: "Normal" });
+inventario.push({ nombre: "Casco", oro: 200, categoria: "Especial" });
+inventario.push({ nombre: "Lanza", oro: 250, categoria: "Legendario" });
 
 console.log(inventario);
 
@@ -24,4 +24,53 @@ console.log("Producto con categoria Legendaria", cat);
 
 const num = inventario.map((item) => item.nombre);
 
-console.log("nombres de los productos",num);
+console.log("nombres de los productos", num);
+
+//Agregá 3 películas al array. Cada película debe tener: título, director, año y género.
+
+const películas = [];
+
+películas.push({
+  titulo: "Jhon Wick",
+  director: "Chad Stahelski",
+  año: 2014,
+  genero: "Accion",
+});
+películas.push({
+  titulo: "Guerra Mundial Z",
+  director: "Marc Forster",
+  año: 2013,
+  genero: "Accion",
+});
+películas.push({
+  titulo: "La Teoria Del Todo",
+  director: "James Marsh",
+  año: 2014,
+  genero: "Drama Biografico",
+});
+
+console.log(películas);
+
+//Agregá una película al inicio del array.
+
+películas.unshift({
+  titulo: "Blade Runner 2049",
+  director: "Denis Villeneuve",
+  año: 2017,
+  genero: "Ciencia Ficción ",
+});
+
+console.log(películas);
+
+//Eliminá la primera película del array y guardala en una variable.
+
+const primero = películas.shift();
+console.log("Primera Pelicula", primero);
+
+console.log(películas);
+
+//Mostrá cuántas películas hay actualmente en el array.
+
+const peliculasAct = películas.map((n) => n.titulo);
+
+console.log("Actualmente estan estas peliculas", peliculasAct);
